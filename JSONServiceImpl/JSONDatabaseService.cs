4412800181756bc9093b4756d4fba0217782fce6
Borkanie <bogdanboboc1789@yahoo.com>
2024-07-27 20:32:14 +0300

    Adds a basic service

---
+++ JSONServiceImpl/JSONDatabaseService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class JSONDatabaseService
+{
+	private string _dataBasePath = "databse.json";
+	private JSONDatabaseService()
+	{
+
+	}
+	private JSONDatabaseService _instance;
+	public JSONDatabaseService instance 
+	{ 
+		get 
+		{ 
+			if (_instance != null) 
+				_instance = new JSONDatabaseService();
+			
+			return _instance; 
+		} 
+	}
+}
