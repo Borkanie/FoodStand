e83e7e939a8150e37ef59381e0550bd51938b7f2
borkanie <bogdanboboc1789@yahoo.com>
2024-08-06 00:05:44 +0300

    Adds a new UI project with corect format

--- FoodMeasuringStandUI/MainActivity.cs
+++ FoodMeasuringStandUI/MainActivity.cs
@@ -1,8 +1,11 @@
+using FoodMeasuringAPI;
+
 namespace FoodMeasuringStandUI
 {
     [Activity(Label = "@string/app_name", MainLauncher = true)]
     public class MainActivity : Activity
     {
+        
         protected override void OnCreate(Bundle? savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
---
+++ FoodStandUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FoodStandUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
