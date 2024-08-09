d798fc84115b1d28bf04c715328f7a84a1e30bc7
borkanie <bogdanboboc1789@yahoo.com>
2024-08-09 16:30:01 +0300

    Adds Item View Model.

---
+++ FoodStandUI/ViewModel/Components/ItemViewModel.cs
@@ -0,0 +1,77 @@
+﻿using FoodMeasuringObjects.Foods;
+using FoodMeasuringObjects.Orders;
+using FoodStandUI.ViewModel.Basic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodStandUI.ViewModel.Components
+{
+    internal class ItemViewModel : BasicViewModel
+    {
+        Item model;
+
+        public ItemViewModel(Item model)
+        {
+            this.model = model;
+        }
+
+        public int Cost
+        {
+            get
+            {
+                return model.Cost;
+            }
+        }
+
+        public Food Food
+        {
+            get
+            {
+                return model.Food;
+            }
+            set
+            {
+                if (model.Food != value)
+                {
+                    model.Food = value;
+                    this.RaisePropertyChanged();
+                }
+            }
+        }
+
+        public int Quantity
+        {
+            get
+            {
+                return model.Quantity;
+            }
+            set
+            {
+                if (model.Quantity != value)
+                {
+                    model.Quantity = value;
+                    this.RaisePropertyChanged();
+                }
+            }
+        }
+
+        public FoodPortioningType Type
+        {
+            get
+            {
+                return model.Type;
+            }
+            set
+            {
+                if (model.Type != value)
+                {
+                    model.Type = value;
+                    this.RaisePropertyChanged();
+                }
+            }
+        }
+    }
+}
